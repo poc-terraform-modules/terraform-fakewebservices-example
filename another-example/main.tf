bd847aa627374a4e6624bc8964d3b9190ad267a2
Leonardo Badia <leonardo.badia@mercadolibre.com>
2022-01-25 17:15:22 -0300

    Test with 2 modules

---
+++ another-example/main.tf
@@ -0,0 +1,18 @@
+resource "fakewebservices_vpc" "primary_vpc" {
+  name       = var.vpc_name
+  cidr_block = "0.0.0.0/1"
+}
+
+resource "fakewebservices_server" "servers" {
+  count = 2
+
+  name = "Server ${count.index + 1}"
+  type = "t2.micro"
+  vpc  = fakewebservices_vpc.primary_vpc.name
+}
+
+resource "fakewebservices_load_balancer" "primary_lb" {
+  name    = var.load_balancer_name
+  servers = fakewebservices_server.servers[*].name
+}
+
---
+++ another-example/variables.tf
@@ -0,0 +1,10 @@
+variable "vpc_name" {
+  description = "Name of the vpc."
+  type        = string
+}
+
+variable "load_balancer_name" {
+  description = "Name of the load balancer"
+  type        = string
+}
+
